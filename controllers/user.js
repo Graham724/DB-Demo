8786714f5dfde4c3e1ab69a6f39f09bfb6f6f46c
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-22 14:18:32 -0500

    finished class update

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,51 @@
 const router = require('express').Router()
 const User = require('../models/user')
 
-router.get('/', (req, res) => {
-    res.send('USER')
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({ "message": String(error) })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findOne({ _id: id })
+
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({ "message": String(error) })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findOneAndDelete({ _id: id })
+        
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({ "message": String(error) })
+    }
 })
 
 router.post('/create', async (req, res) => {
-    const { username, password, age, name } = req.body
+    try {
+        const { username, password, age, name } = req.body
     const createdUser = await new User ({
         username,
         password,
         age,
         name
     }).save()
+    } catch (error) {
+        console.log("ERROR: ", error)
+        res.status(400).json({ "message": String(error) })
+    }
 
     res.send(createdUser)
 })
